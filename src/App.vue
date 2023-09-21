1d902bb4da84e1fe16b220b4f58f882c875bd20a
wea_ondara <wea_ondara@cutiepie.at>
2023-09-21 18:20:35 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts"  >
+import {RouterView} from 'vue-router';
+import {Options, Vue} from 'vue-class-component';
+
+@Options({
+  name: 'App',
+  components: {
+    RouterView,
+  },
+})
+export default class App extends Vue {
+}
+
+</script>
+
+<template>
+  <RouterView/>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({
+  name: 'HomeView',
+})
+export default class HomeView extends Vue {
+}
+</script>
+
+<template>
+  main
+</template>
