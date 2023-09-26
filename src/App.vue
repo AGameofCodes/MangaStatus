79d71e82c4121c877481528e171700416d8c42ca
wea_ondara <wea_ondara@cutiepie.at>
2023-09-26 16:33:03 +0200

    added LocaleSaver.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import {RouterView} from 'vue-router';
 import {Options, Vue} from 'vue-class-component';
 import DocumentLocaleSetter from '@/components/locale/DocumentLocaleSetter.vue';
 import NavBar from '@/components/NavBar.vue';
+import LocaleSaver from '@/components/locale/LocaleSaver.vue';
 
 @Options({
   name: 'App',
   components: {
     NavBar,
     DocumentLocaleSetter,
+    LocaleSaver,
     RouterView,
   },
 })
@@ -19,6 +21,7 @@ export default class App extends Vue {
 
 <template>
   <div class="d-flex flex-column h-100 w-100">
+    <LocaleSaver/>
     <DocumentLocaleSetter/>
     <NavBar/>
     <RouterView class="flex-grow-1"/>
