5c3e7a3b6864ad95e513a2f580ee7d24f9c63167
wea_ondara <wea_ondara@cutiepie.at>
2024-06-04 20:14:51 +0200

    update vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import {fileURLToPath, URL} from 'node:url';
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import {ManualChunkMeta} from 'rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,13 @@ export default defineConfig({
     emptyOutDir: false,
     rollupOptions: {
       input: ['./src/main.ts', './index.html'],
+      output: {
+        manualChunks: (id: string, meta: ManualChunkMeta) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
     },
   },
   clearScreen: false,
