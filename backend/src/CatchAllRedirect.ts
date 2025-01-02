21e97eb3789927660e9ef4f11694c563624942bf
wea_ondara <wea_ondara@wea-ondara.net>
2025-01-02 14:10:47 +0100

    fix catch-all-redirect

--- backend/src/CatchAllRedirect.ts
+++ backend/src/CatchAllRedirect.ts
@@ -2,6 +2,11 @@ import {Express, NextFunction, Request, RequestHandler, Response} from 'express'
 
 export default function catchAllRedirect(app: Express, path: string): RequestHandler {
   return (req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) { //already processed
+      next();
+      return;
+    }
+
     req.url = path;
     (app as any).handle(req, res); //handle() is not exposed by .d.ts files
   };
